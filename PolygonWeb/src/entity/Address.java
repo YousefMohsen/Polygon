af5cb30da4ef6b826932e63662b352c85ef003a4
Joacim Vetterlain <jvetterlain@jumbotransport.dk>
2016-11-23 14:01:35 +0100

    fixed yousef

---
+++ PolygonWeb/src/entity/Address.java
@@ -0,0 +1,52 @@
+package entity;
+
+public class Address {
+
+    private int addressID;
+    private String addressline;
+    private ZipCode zipCode;
+
+    public Address() {
+    }
+
+    public Address(String addressline, ZipCode zipCode) {
+        this.addressline = addressline;
+        this.zipCode = zipCode;
+    }
+
+    public Address(int addressID, String addressline, ZipCode zipCode) {
+        this.addressID = addressID;
+        this.addressline = addressline;
+        this.zipCode = zipCode;
+    }
+
+    public int getAddressID() {
+        return addressID;
+    }
+
+    public void setAddressID(int addressID) {
+        this.addressID = addressID;
+    }
+
+    public String getAddressline() {
+        return addressline;
+    }
+
+    public void setAddressline(String addressline) {
+        this.addressline = addressline;
+    }
+
+    public ZipCode getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(ZipCode zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString() {//delete
+        return "Address{" + "addressID=" + addressID + ", addressline=" + addressline + ", zipCode=" + zipCode + '}';
+    }
+
+}
---
+++ PolygonWeb/src/entity/User.java
@@ -0,0 +1,81 @@
+package entity;
+
+public class User {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private String email;
+    private Address address;
+
+    public User() {
+    }
+
+    public User(int id, String firstname, String lastname, String phone, String email, Address address) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public User(String firstname, String lastname, String phone, String email, Address address) {
+        // this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+}
