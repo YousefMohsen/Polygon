69f545fc920d6ef96958d4a2053c5825a4b81b3f
Zaeem-Shafiq <zaeem1996@hotmail.com>
2016-11-24 21:19:48 +0100

    Added FloorPlan.html and new servlet

---
+++ PolygonWeb/src/servlet/UploadServlet.java
@@ -0,0 +1,111 @@
+package servlet;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+@WebServlet(name = "UploadServlet", urlPatterns = {"/UploadServlet"})
+@MultipartConfig
+public class UploadServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            response.sendRedirect("index.jsp");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            // get access to file that is uploaded from client
+//            Part p = request.getPart("file");
+//            InputStream is = p.getInputStream();
+
+            // get filename to use on the server
+            //String outputfile = this.getServletContext().getRealPath("/floorPlan/");  // get path on the server
+            //FileOutputStream os = new FileOutputStream (outputfile + "TEST.jpg");
+            
+            Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
+            String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+            InputStream is = filePart.getInputStream();
+            File uploads = new File();
+            out.println(uploads);
+            FileOutputStream os = new FileOutputStream (uploads + "TEST.jpg");
+            
+            // write bytes taken from uploaded file to target file
+            int ch = is.read();
+            while (ch != -1) {
+                 os.write(ch);
+                 ch = is.read();
+            }
+            os.close();
+            out.println("<h3>File uploaded successfully!</h3>");
+        }
+        catch(Exception ex) {
+           out.println("Exception -->" + ex.getMessage());
+        }
+        finally { 
+            out.close();
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
