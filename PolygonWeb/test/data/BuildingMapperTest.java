a236a0ab09a23acd362fb797f154cb90f1723331
JanusU <janusudsen@yahoo.com>
2016-11-28 13:29:02 +0100

    added some more test

---
+++ PolygonWeb/test/data/BuildingMapperTest.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Janus
+ */
+public class BuildingMapperTest {
+    
+    public BuildingMapperTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of createBuilding method, of class BuildingMapper.
+     */
+    @Test
+    public void testCreateBuilding() {
+    }
+
+    /**
+     * Test of getBuildings method, of class BuildingMapper.
+     */
+    @Test
+    public void testGetBuildings() {
+    }
+
+    /**
+     * Test of getBuildingsForUser method, of class BuildingMapper.
+     */
+    @Test
+    public void testGetBuildingsForUser() {
+    }
+
+    /**
+     * Test of getBuilding method, of class BuildingMapper.
+     */
+    @Test
+    public void testGetBuilding() {
+    }
+
+    /**
+     * Test of updateBuilding method, of class BuildingMapper.
+     */
+    @Test
+    public void testUpdateBuilding() {
+    }
+
+    /**
+     * Test of loadZip method, of class BuildingMapper.
+     */
+    @Test
+    public void testLoadZip() {
+    }
+
+    /**
+     * Test of loadAddress method, of class BuildingMapper.
+     */
+    @Test
+    public void testLoadAddress() {
+    }
+
+    /**
+     * Test of findCity method, of class BuildingMapper.
+     */
+    @Test
+    public void testFindCity() {
+    }
+
+    /**
+     * Test of findZipID method, of class BuildingMapper.
+     */
+    @Test
+    public void testFindZipID() {
+    }
+
+    /**
+     * Test of insertAddress method, of class BuildingMapper.
+     */
+    @Test
+    public void testInsertAddress() {
+    }
+
+    /**
+     * Test of getDeletionBuildings method, of class BuildingMapper.
+     */
+    @Test
+    public void testGetDeletionBuildings() {
+    }
+
+    /**
+     * Test of hideBuilding method, of class BuildingMapper.
+     */
+    @Test
+    public void testHideBuilding() {
+    }
+    
+}
