5417022fe1c3bc94e056385f0f64f8007f239a77
JanusU <janusudsen@yahoo.com>
2016-11-28 13:55:07 +0100

    made connection a singleton with injection functionality for testing

--- PolygonWeb/test/data/BuildingMapperTest.java
+++ PolygonWeb/test/data/BuildingMapperTest.java
@@ -5,6 +5,10 @@
  */
 package data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -18,8 +22,28 @@ public class BuildingMapperTest {
     public BuildingMapperTest() {
     }
     
+    private Connection testDatabase;
+    private static String USER = "javatester";
+    private static String USERPW = "testogeksperimenter=21coolstuf";
+    private static String DBNAME = "buildingsTest";
+    
     @Before
-    public void setUp() {
+    public void setUp() { try {
+            String url = String.format("jdbc:mysql://vetterlain.dk/Polygon");
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            testDatabase =  DriverManager.getConnection(url, USER, USERPW);
+            try( Statement stmt = testDatabase.createStatement()){
+                stmt.execute( "drop table if exists building");
+                stmt.execute( "create table building like testbuilding");
+                stmt.execute( "insert into building select * from testbuilding");
+            }
+            DB.setConnection(testDatabase);
+            
+        } catch ( ClassNotFoundException | SQLException ex ) {
+            testDatabase = null;
+            System.out.println( "Could not open connection to database: " + ex.getMessage() );;
+        }
     }
 
     /**
