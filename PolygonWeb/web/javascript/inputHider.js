e5acb2182cea6cf3b7ecaa8607761d59b7b348a5
Joacim Vetterlain <Joacim@Vetterlain.dk>
2016-11-28 14:48:06 +0100

    changed a little bit of js

--- PolygonWeb/web/javascript/inputHider.js
+++ PolygonWeb/web/javascript/inputHider.js
@@ -1,13 +1,12 @@
 var inputs = document.getElementsByTagName("INPUT");
 function lock() {
     for (var i = 0; i < inputs.length; i++) {
-        inputs[i].disabled = true;
+        inputs[i].disabled = true;       
     }
 }
 function unlock() {
     for (var i = 0; i < inputs.length; i++) {
-        inputs[i].disabled = false;
-        console.log("test");
+        inputs[i].disabled = false;      
     }
 }
 
